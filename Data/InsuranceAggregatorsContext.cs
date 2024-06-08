cb60bd3a10c877fa59f0d8cadc8d6c52e46c0391
Atheer <Alenazi.Atheer120@gmail.com>
2024-06-08 22:25:05 +0300

    add Register and Login Part

---
+++ Data/InsuranceAggregatorsContext.cs
@@ -0,0 +1,15 @@
+﻿using InsuranceAggregator.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceAggregator.Data
+{
+    public class InsuranceAggregatorsContext:IdentityDbContext<ApplicationUser>
+    {
+        public InsuranceAggregatorsContext(DbContextOptions<InsuranceAggregatorsContext> options):base(options)
+        {
+            
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,35 @@
+using InsuranceAggregator.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<InsuranceAggregatorsContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:DefaultConnection"]);
+});
+builder.Services.AddControllersWithViews();//for working with view interface..
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<InsuranceAggregatorsContext>()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Account}/{action=Login}/{id?}");
+});
 
 app.Run();
